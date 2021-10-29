ceba2a9d7366c4838e31fb3ae6ab497612426c31
Alec Sharp <alecsharpie@gmail.com>
2021-10-29 22:21:34 +1100

    finalise colourscheme

--- wakematch/create_graph.py
+++ wakematch/create_graph.py
@@ -10,8 +10,8 @@ def create_graph(df, user_timezone):
                       x_end="end",
                       y="person",
                       range_x=get_limits(user_timezone),
-                      color_discrete_sequence=['#003CBE'],
-                      opacity=0.3,
+                      color_discrete_sequence=['#FCB01C'],
+                      opacity=0.9,
                       hover_name=None,
                       hover_data={
                           'tz': False,
@@ -20,13 +20,14 @@ def create_graph(df, user_timezone):
                           'person': False
                       })
 
-    for idx, row in find_waketimes(df, user_timezone).iterrows():
-        fig.add_vrect(x0=row['start'],
-                      x1=row['end'],
-                      fillcolor="#FCD221",
-                      opacity=0.5,
-                      layer="above",
-                      line_width=1)
+    if len(df.tz.unique()) > 1:
+        for idx, row in find_waketimes(df, user_timezone).iterrows():
+            fig.add_vrect(x0=row['start'],
+                        x1=row['end'],
+                        fillcolor="#008CEF",
+                        opacity=0.5,
+                        layer="above",
+                        line_width=1)
 
     # ({row['start'].astimezone(pytz.timezone('Europe/Zurich')).strftime('%I%p, %d %b, %Y')} and {row['end'].astimezone(pytz.timezone('Europe/Zurich')).strftime('%I%p, %d %b, %Y')} their time)
 
