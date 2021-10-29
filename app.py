213d6faa6d614dfaaa700eb4c2345d8c3f5f5bf6
Alec Sharp <alecsharpie@gmail.com>
2021-10-30 00:07:20 +1100

    remove print statments

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+server = app.server
+
 app.layout = html.Div([
     html.Div([
         html.H1(children='WakeTimes', style={'textAlign': 'center'}),
